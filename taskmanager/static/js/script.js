cb06223a0740c9abd2de93e16c93a6212ec70981
EvergSwe <daniel.nyberg@everg.se>
2022-11-05 14:48:06 +0000

    basic html added

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //side nav initialization
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
